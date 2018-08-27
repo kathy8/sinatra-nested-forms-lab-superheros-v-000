dd0a5f20c125463188906b584fd1caa951d89328
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-08-27 18:03:23 +0000

    automatically backed up by learn

---
+++ app/models/member.rb
@@ -0,0 +1,16 @@
+class member
+  attr_accessor :name, :power, :bio
+
+  @@all = []
+
+  def initialize(params)
+
+@name = params[name]
+@power = params[power]
+@bio = params[bio]
+end
+
+def self.all
+  @@all
+end
+end
