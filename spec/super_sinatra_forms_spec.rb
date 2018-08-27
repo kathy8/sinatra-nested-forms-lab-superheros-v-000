92e417af077c1ef8d6e7ebcb2d44428c6d72c8b6
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-08-27 18:19:39 +0000

    automatically backed up by learn

--- spec/super_sinatra_forms_spec.rb
+++ spec/super_sinatra_forms_spec.rb
@@ -33,7 +33,7 @@ describe App do
 
       expect(page).to have_text("Team Ruby")
       expect(page).to have_text("Team Motto: We love Ruby!")
-      expect(page).to have_text("Hero Name: Amanda")
+      expect(page).to have_text("Hero Name: Katie")
       expect(page).to have_text("Hero Power: JavaScript")
       expect(page).to have_text("Hero Biography: I love JavaScript!")
       expect(page).to have_text("Hero Name: Arel")
