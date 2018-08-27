d10917b1d810b64195fb203bb0ff8c28f07c296f
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-08-27 17:52:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    get '/' do
+      erb :super_hero
+    end
 
+    post '/team' do
+      @team = Team.new(params[:team])
+      params[:team][:members].each do |members|
+        Member.new(member)
+      end
+
+      @members = Members.all
+      erb :team
+
+  
+end
 
 end
